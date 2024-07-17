56b4dc9c8b2ec70dddac9cec6ad0a41fca9b7784
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2024-07-17 01:15:46 -0300

    Add backend to insert latest ARS blue dollar rate to psql

---
+++ backend/app/insert_latest_blue_value.py
@@ -0,0 +1,52 @@
+import os
+from sqlalchemy import create_engine, Column, Integer, String, JSON
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+from utils.fetch_latest_blue import fetch_latest_blue
+
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Get the database URL from the environment variables
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+# Define the base
+Base = declarative_base()
+
+# Define the APIData model
+
+
+class APIData(Base):
+    __tablename__ = 'value_ars'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    datetime = Column(String)
+    value = Column(Integer)
+
+
+# Create an engine
+engine = create_engine(DATABASE_URL)
+
+# Create the table
+Base.metadata.create_all(engine)
+
+# Create a configured "Session" class
+Session = sessionmaker(bind=engine)
+
+# Create a session
+session = Session()
+
+# Get data
+last_price, last_update = fetch_latest_blue()
+
+# Create an instance of the APIData model
+api_data_instance = APIData(datetime=last_update, value=last_price)
+
+# Add the instance to the session
+session.add(api_data_instance)
+
+# Commit the transaction
+session.commit()
+
+# Close the session
+session.close()
---
+++ backend/app/utils/fetch_latest_blue.py
@@ -0,0 +1,11 @@
+from utils.fetch_latest_data import fetch_latest_data
+
+
+def fetch_latest_blue():
+    _json = fetch_latest_data()
+    last_price = _json['blue']['value_avg']
+    last_update = _json['last_update']
+
+    print(f'Fetched fetch_latest_blue: [value_avg:{last_price}, last_update: {last_update}]')
+
+    return last_price, last_update
