3f21cdc8f672fea42c12f2dbdc43b641a5cb063a
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2024-07-30 14:01:09 -0300

    Move border to col

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,8 +6,8 @@ import { ExchangeRateProvider } from "@/contexts/ExhangeRateContext";
 export default function Home() {
   return (
     <div className="container mx-auto m-4" style={{ maxWidth: "800px" }}>
-      <div className="row border pt-2">
-        <div className="col">
+      <div className="row pt-2">
+        <div className="col border mx-2">
           <h1 className="p-2 mb-2">Dollar Blue</h1>
           <ExchangeRateProvider>
             <LatestRateDisplay />
