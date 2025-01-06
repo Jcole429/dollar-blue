2c0442453bfdfebdc8b15853b3ad46d4ec2609bc
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2025-01-06 13:31:28 -0300

    Only use console handler

--- backend/logger.py
+++ backend/logger.py
@@ -9,11 +9,16 @@ def setup_logger():
     file_handler = logging.FileHandler("app.log")
     file_handler.setLevel(logging.INFO)
 
+    # Console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
     # Formatter
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
 
-    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
     return logger
 
 
