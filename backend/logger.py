a960b70252cbc03ef6ed06fb3e6fc3f4840a5a19
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2025-01-06 13:37:23 -0300

    Remove file handler

--- backend/logger.py
+++ backend/logger.py
@@ -5,17 +5,12 @@ def setup_logger():
     logger = logging.getLogger("dollar-blue")
     logger.setLevel(logging.INFO)
 
-    # File handler
-    file_handler = logging.FileHandler("app.log")
-    file_handler.setLevel(logging.INFO)
-
     # Console handler
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
 
     # Formatter
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
 
     logger.addHandler(console_handler)
