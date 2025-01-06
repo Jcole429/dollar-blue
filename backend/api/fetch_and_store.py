d63f5d8e0b66578aeb03fcf8218f6f6055063178
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2025-01-06 14:03:26 -0300

    Update backend API method from vercel docs

--- backend/api/fetch_and_store.py
+++ backend/api/fetch_and_store.py
@@ -1,18 +1,25 @@
 from fetch_data import fetch_latest_blue, fetch_latest_crypto
 from db_utils import check_and_insert_data
 from logger import logger
+from http.server import BaseHTTPRequestHandler
 
 
-def handler(request):
-    try:
-        data_1 = fetch_latest_blue()
-        data_2 = fetch_latest_crypto()
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            data_1 = fetch_latest_blue()
+            data_2 = fetch_latest_crypto()
 
-        # Store in the database
-        check_and_insert_data(data_1["type"], data_1["source"], data_1["updated_date"], data_1["buy"], data_1["sell"])
-        check_and_insert_data(data_2["type"], data_2["source"], data_2["updated_date"], data_2["buy"], data_2["sell"])
-
-        return {"statusCode": 200, "body": "Data fetched and stored successfully."}
-    except Exception as e:
-        logger.error(f"Error during fetch and store: {e}")
-        return {"statusCode": 500, "body": "Internal Server Error"}
+            # Store in the database
+            check_and_insert_data(data_1["type"], data_1["source"], data_1["updated_date"], data_1["buy"], data_1["sell"])
+            check_and_insert_data(data_2["type"], data_2["source"], data_2["updated_date"], data_2["buy"], data_2["sell"])
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('Data fetched and stored successfully.'.encode('utf-8'))
+        except Exception as e:
+            error = f"Error during fetch and store: {e}"
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(f'Internal Server Error\n{error}'.encode('utf-8'))
